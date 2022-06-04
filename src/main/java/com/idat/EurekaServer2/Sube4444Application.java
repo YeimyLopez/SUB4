c9e961539cd0071bfc1ddb2fa24f3571181be1bb
Piero Nicolas Quispe Pozo <pieronicolas333@gmail.com>
2022-06-03 21:42:32 -0500

    evaluacion4444

---
+++ src/main/java/com/idat/EurekaServer2/Sube4444Application.java
@@ -0,0 +1,13 @@
+package com.idat.EurekaServer2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sube4444Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sube4444Application.class, args);
+	}
+
+}
